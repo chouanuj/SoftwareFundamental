4afefea065abac7cde65917259de99fcbc2a693b
chouanuj <chouanuj@sheridancollege.ca>
2019-05-17 14:42:14 -0400

    2nd commit

--- src/ca/sheridancollege/week1/softwarefundamentals/StudentList.java
+++ src/ca/sheridancollege/week1/softwarefundamentals/StudentList.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package ca.sheridancollege.week1.softwarefundamentals;
-
+import java.util.Scanner;
 /**
  *
  * @author sidhu376
@@ -12,19 +12,25 @@ package ca.sheridancollege.week1.softwarefundamentals;
 public class StudentList {
     
     public static void main(String[] args) {
-        
+        String name;
+        int id;
+        Scanner in=new Scanner(System.in);
         Student [] sList= new Student[5];
         
         for (int i=0;i<sList.length;i++)
         {
             sList[i]=new Student();
-            sList[i].setName("Student" +(i+1));
+            
         }
         
         for(int i=0;i<sList.length;i++)
         {
-            System.out.println(sList[i].getName());
-          
+            System.out.println("Enter name");
+            name=in.next();
+            sList[i].setName("Student" +(name));
+            System.out.println("Enter id");
+            id=in.nextInt();
+            sList[i].setId(id);
         }
         
     }
