ae5b542a2c918214614534477acc034c11ec8bfd
chouanuj <50750050+chouanuj@users.noreply.github.com>
2019-05-24 13:35:34 -0400

    Update Student.java

--- src/ca/sheridancollege/week1/softwarefundamentals/Student.java
+++ src/ca/sheridancollege/week1/softwarefundamentals/Student.java
@@ -12,7 +12,7 @@ package ca.sheridancollege.week1.softwarefundamentals;
 public class Student {
     
     private String name;
- //int abc;
+ //int abcd;
     /**
      * Get the value of name
      *
